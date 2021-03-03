4e55c6e7987ebdb0e35bbb3d08d30e48c6328165
KounLext <74634880+KounLext@users.noreply.github.com>
2021-03-03 09:22:11 +0300

    Add files via upload

---
+++ scripts/discount_types_init.sql
@@ -0,0 +1,5 @@
+INSERT INTO shop.discount_types 
+VALUES 
+(1,'Все товары'),
+(2,'Группа товаров'),
+(3,'Товар');
---
+++ scripts/quantity_types_init.sql
@@ -0,0 +1,6 @@
+INSERT INTO shop.quantity_types
+VALUES 
+(1, 'шт'),
+(2, 'кг'),
+(3, 'гр'),
+(4, 'м');
