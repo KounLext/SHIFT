fadbe39a8750410c5ec561c5e28b36fa29ce319c
KounLext <74634880+KounLext@users.noreply.github.com>
2021-03-03 07:59:19 +0300

    Delete shops_warehouse.sql

--- shops_warehouse.sql
+++
@@ -1,9 +0,0 @@
-create table shop.shops_warehouse
-(
-    id               serial not null
-        constraint shops_warehouse_pkey
-            primary key,
-    shop_id          integer,
-    product_id       integer,
-    quantity         integer
-)
