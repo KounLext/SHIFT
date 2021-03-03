9aa75dd02f49a4b114dd79aa5b09f5803ac2ea98
KounLext <74634880+KounLext@users.noreply.github.com>
2021-03-03 07:58:48 +0300

    Delete products_groups.sql

--- products_groups.sql
+++
@@ -1,8 +0,0 @@
-create table shop.products_groups
-(
-    id              serial not null
-        constraint products_groups_pkey
-            primary key,
-    group_name      varchar,
-    parent_group_id integer
-);
