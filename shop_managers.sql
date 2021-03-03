203c00f75f86449cde53c1a0a13caafbaeab7ccd
KounLext <74634880+KounLext@users.noreply.github.com>
2021-03-03 07:59:04 +0300

    Delete shop_managers.sql

--- shop_managers.sql
+++
@@ -1,8 +0,0 @@
-create table shop.shop_managers
-(
-    id               serial not null
-        constraint shop_managers_pkey
-            primary key,
-    shop_id         integer,
-    manager_id       integer
-)
